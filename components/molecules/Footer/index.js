06a647cc93a5fbcc2b81cf45b5243ee2aafb1bf8
Fabricio Murillo <fabriciomurillo24@gmail.com>
2021-03-27 18:06:31 -0600

    FIX Logo

--- components/molecules/Footer/index.js
+++ components/molecules/Footer/index.js
@@ -15,7 +15,21 @@ const Footer = () => {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <Link href="/">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <Logo />
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              </svg>
+              <span className="ml-3 text-xl">Bug Tracker</span>
+            </>
           </a>
         </Link>
 
--- pages/login/index.js
+++ pages/login/index.js
@@ -67,7 +67,21 @@ const Login = () => {
           <>
             <Link href="/">
               <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <Logo />
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                  </svg>
+                  <span className="ml-3 text-xl">Bug Tracker</span>
+                </>
               </a>
             </Link>
             <Typography.Text>{`Hey there!  :)`}</Typography.Text>
